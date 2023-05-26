b8f2631c999928dd466ac4f2a6b30960cd4aa9b4
Suman Shrestha <summonshr@gmail.com>
2023-05-26 17:55:04 +0545

    Updated

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cd /root/personal
 # Get today's date
-date=$(date +'%Y-%m-%d')
-
+date=$(date +'%Y-%m-%d %H:00:00')
+git pull
 # Update README.md file
+sed -i '$d' index.html
+echo "<!-- Last updated at $date -->" > index.html
 echo "Last Updated att $date" > README.md
-git add README.md
+git add . 
 git commit -m "Updated"
 git push
+
