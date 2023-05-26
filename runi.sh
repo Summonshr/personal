e9d97a4816b1fbf70c186e6afa36017b7d93ebc5
Suman Shrestha <summonshr@gmail.com>
2023-05-26 17:56:14 +0545

    Updated

--- runi.sh
+++ runi.sh
@@ -5,7 +5,7 @@ date=$(date +'%Y-%m-%d %H:00:00')
 git pull
 # Update README.md file
 sed -i '$d' index.html
-echo "<!-- Last updated at $date -->" > index.html
+echo "<!-- Last updated at $date -->" >> index.html
 echo "Last Updated att $date" > README.md
 git add . 
 git commit -m "Updated"
